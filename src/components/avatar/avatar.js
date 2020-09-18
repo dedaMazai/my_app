d8e3dda1d242702020e9b277ed51d7d30d406348
Андрей Юрьевич <andryukha.chipizybov@mail.ru>
2020-09-18 22:20:22 +0300

    i developed avater block

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react';
 
 import './avatar.css';
 export default class Avatar extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
     }
     render() {
         return (
             <div className="avatar">
-                <img  className="avatar" src="https://ktonanovenkogo.ru/image/finik.jpg" title="1" alt="Image preview..."/>
-                <div className="avafooter">
-                    <p className="avatar level">Золотой уровень:</p>
-                    <p className="avatar status">Постоялец:</p>
-                    <p className="avatar history">История заказов</p>
-                    <p className="avatar warn">Предупреждение!</p>
-                </div>
+                <img  className="avatar" src="http://zornet.ru/_ld/93/48255040.jpg" title="1" alt="Image preview..."/>
+                <ul className="avafooter">
+                    <li className="level">Золотой уровень: <strong>+60р</strong></li>
+                    <li className="status">Постоялец: <strong>+5р</strong></li>
+                    <li className="history">История заказов</li>
+                    <li className="warn">Предупреждение!</li>
+                </ul>
             </div>
         )
     }
