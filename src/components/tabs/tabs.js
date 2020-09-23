64cc926fa66356a83093abbf0cf59e29664dfa97
Андрей Юрьевич <andryukha.chipizybov@mail.ru>
2020-09-23 23:19:19 +0300

    i developed stule tabs on main page

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -2,32 +2,26 @@ import React, {Component} from 'react';
 
 import './tabs.css';
 export default class Tabs extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <ul className="tabs">
                 <li className="tab tab1">
                     <p>1.Аватарка</p>
-                    <br/>
                     <h1>60%</h1>
-                    <p>от заказа</p>
+                    <h3>от заказа</h3>
                 </li>
                 <li className="tab tab2">
                     <p>1.Аватарка</p>
                     <p>2.Ссылка в статусе</p>
                     <p>3.Банер на стене с ежедневным обновлением</p>
-                    <br/>
                     <h1>100%</h1>
-                    <p>от заказа</p>
+                    <h3>от заказа</h3>
                 </li>
                 <li className="tab tab3">
                     <p>1.Аватарка</p>
                     <p>2.Ссылка в статусе</p>
-                    <br/>
                     <h1>65%</h1>
-                    <p>от заказа</p>
+                    <h3>от заказа</h3>
                 </li>
             </ul>
         )
